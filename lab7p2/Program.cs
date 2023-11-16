10b992b37c93c7d0da534122f67b39d9aface607
Minerv04ka <oleksandr.zhuk.2000@gmail.com>
2023-11-16 15:52:48 +0200

    first commit

---
+++ lab7p2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class TextProcessor
+{
+    public string InputText { get; set; }
+
+    public TextProcessor(string inputText)
+    {
+        InputText = inputText;
+    }
+
+    public int CalculateLength()
+    {
+        return InputText.Length;
+    }
+
+    public string ConvertToUpperCase()
+    {
+        return InputText.ToUpper();
+    }
+
+    public string ReverseText()
+    {
+        char[] charArray = InputText.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть рядок тексту: ");
+        string input = Console.ReadLine();
+
+        TextProcessor processor = new TextProcessor(input);
+
+        Console.WriteLine($"Довжина введеного тексту: {processor.CalculateLength()}");
+        Console.WriteLine($"Рядок тексту у верхньому регістрі: {processor.ConvertToUpperCase()}");
+        Console.WriteLine($"Рядок тексту навпаки: {processor.ReverseText()}");
+
+        Console.ReadKey();
+    }
+}
